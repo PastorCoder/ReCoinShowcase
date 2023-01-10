05aefbc519bfef80110d4afc6009209adf27b3ae
pastorcoder <zuma4good@gmail.com>
2023-01-10 23:16:12 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import { Download, Features, SectionWrapper } from "./components";
+
+const App = () => {
+  return (
+    <div className="text-3x1 font-bold underline">
+        <h1>
+          React Native NFT Showcase
+        </h1>
+        <Download />
+        <Features />
+        <SectionWrapper />
+    </div>
+  );
+}
+
+export default App;
